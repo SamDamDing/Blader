acd2b7702373cf0cc100461239f4e081bec1ed5f
SamDamDing <45549722+SamDamDing@users.noreply.github.com>
2021-12-06 06:52:43 -0500

    Update H3ShaderBitmapTool Modify and Run 3rd.py

--- H3ShaderBitmapTool Modify and Run 3rd.py
+++ H3ShaderBitmapTool Modify and Run 3rd.py
@@ -7,19 +7,14 @@ import glob
 uppersource_str = '5F5F5F5F5F5F5F5F5F5F5F5F5F5F5F5F5F5F5F5F5F5F5F5F5F5F5F5F5F5F5F5F5F5F5F5F5F5F5F5F'    #The name of the bitmap in the shader. 36 characters long.
 source_str=uppersource_str.lower()
 charlimit = 40
-#*.shader
 default_directory='F://SteamLibrary//steamapps//common//H3EK//tags//mercymoon//objects//levels//shared//suzanne//shaders//' #The directory with your blader generated shaders
 for i in glob.glob(default_directory + '*.shader', recursive=True): #Goes through all the shader files in this directory
     filename=os.path.basename(i).lower() #Gets the file name
     basename=os.path.splitext(filename)[0] #Strips the .shader
     replace_str=basename.encode('utf-8') #encodes the stripped file name in utf-8
     replace_str=replace_str.hex() #converts the file name into hex
-    #print(source_str)
     with open(i, 'rb') as f:
         content = f.read().hex() #Reads the shader
-    #print(source_str + " in `shader`:       ", source_str in content)
-    #print(replace_str)
-    #print(replace_str in content)
     print(source_str in content)
     print(len(replace_str))
     if source_str in content and len(replace_str) == 2*charlimit:
